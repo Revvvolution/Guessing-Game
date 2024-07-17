e6b98f271f856e4a007f86babe48a3f0f5ce6c9c
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-07-17 14:24:12 -0400

    Phase 3 Complete

--- Program.cs
+++ Program.cs
@@ -13,25 +13,30 @@ namespace NumberGuessingGame
             Console.WriteLine(" -------------------------\n   Number Guessing Game!  \n -------------------------");
             Console.WriteLine("Try to guess the secret number. (HINT: between 1 and 100)\n");
 
-            // Display a prompt for the user's guess
-            Console.Write("Enter your guess: ");
 
-            // Take the user's guess as input and save it as a variable
-            string userInput = Console.ReadLine() ?? string.Empty;  // ?? = null-coalescing operator (to fix error message from phase 1)
+            // Give the user four chances to guess the number.
+            for (int i=0; i<4; i++)
+            {
+                // Display a prompt for the user's guess
+                Console.Write("Enter your guess: ");
 
-            // Convert the user input to an integer
-            int userGuess = int.Parse(userInput!);  // ! = null-forgiving operator (to fix error message from phase 1)
+                // Take the user's guess as input and save it as a variable
+                string userInput = Console.ReadLine() ?? string.Empty;  // ?? = null-coalescing operator (to fix error message from phase 1)
 
-            // Display the user's guess back to the screen (REMOVED IN PHASE 2)
-            // Console.WriteLine($"You guessed: {userGuess}");
+                // Convert the user input to an integer
+                int userGuess = int.Parse(userInput!);  // ! = null-forgiving operator (to fix error message from phase 1)
 
-            if (userGuess == secretNumber) {
-                Console.WriteLine("CORRECT!!! Incredible....how did you know!?");
+                // Display the user's guess back to the screen (REMOVED IN PHASE 2)
+                // Console.WriteLine($"You guessed: {userGuess}");
+
+                if (userGuess == secretNumber) {
+                    Console.WriteLine("CORRECT!!! Incredible....how did you know!?\n");
+                }
+                else{
+                    Console.WriteLine("WRONG!!! I knew my secret number was a good one...\n");
+                }
+
             }
-            else{
-                Console.WriteLine("WRONG!!! I knew my secret number was a good one...");
-            }
-
             // Pause to keep the console window open
             Console.ReadLine();
         }
